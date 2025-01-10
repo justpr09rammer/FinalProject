40530e2fd897bc6d0458f58d0a207edbabadf6ca
nihadgojayev <nihadqocayev152@gmail.com>
2025-01-10 06:06:53 -0800

    first commit

--- src/main/java/com/example/finalproject/FinalProjectApplication.java
+++ src/main/java/com/example/finalproject/FinalProjectApplication.java
@@ -2,10 +2,8 @@ package com.example.finalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-
 public class FinalProjectApplication {
 
     public static void main(String[] args) {
